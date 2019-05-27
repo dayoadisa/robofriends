02fb7143a242c12788662386e0ff9df5063af459
Dayo Adisa <dayoadisa@gmail.com>
2019-05-27 16:26:43 +0300

    updated robofriends app

---
+++ build/precache-manifest.e87853540e460e20ca49a22a03e28a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ac5dbc73c813bed1408",
+    "url": "/static/css/main.6d251f7f.chunk.css"
+  },
+  {
+    "revision": "9ac5dbc73c813bed1408",
+    "url": "/static/js/main.7f3dcdbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66077258a0e07dcce3db",
+    "url": "/static/css/2.bde1a288.chunk.css"
+  },
+  {
+    "revision": "66077258a0e07dcce3db",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9aba6a96f6eb86dc5ada2bcedfd004d7",
+    "url": "/index.html"
+  }
+];
